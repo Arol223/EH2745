33188b127ed11b07c322e762cb8f2cb8e3cbd106
Arol223 <arvid.rolander@gmail.com>
2022-06-01 12:16:26 +0200

    Update KNN.py

--- Assignment2/KNN.py
+++ Assignment2/KNN.py
@@ -11,22 +11,40 @@ import pandas as pd
 from DataLoading import get_all
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
+from numpy.random import randint
 
 
+def data_split(data, prop_test=0.3):
+    data_len = data.shape[0]
+    test_set_len = round(data_len * prop_test)
+    test_inds = randint(0, data_len - 1, size=test_set_len)
+    train_inds = [i for i in range(data_len) if i not in test_inds]
+    train_set = data.iloc[train_inds]
+    test_set = data.iloc[test_inds]
+    return train_set, test_set
+    
+    
 class KNNClassifier:
     
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, *args, **kwargs):
+        if "data" in kwargs.keys():
+            data = kwargs["data"]
+            self.data.iloc[:,:-1] = self.scale_data(data.iloc[:,:-1]) #Don't scale labels, they are in last col by convention
     
     def classify(self, k, point, data=None):
         if data is None:
             data = self.data
     
-    def scale_data(self, data):
-        scaler = MinMaxScaler()
+    def scale_data(self, data, mode="init"):
         data_np = data.to_numpy()
-        data_np = scaler.fit_transform(data)
-        self.scaler = scaler
+        if mode == "init":
+            scaler = MinMaxScaler()
+            data_np = scaler.fit_transform(data_np)
+            self.scaler = scaler
+        elif mode == "use_old": # uses scaler with existing transform 
+            scaler = self.scaler
+            data_np = scaler.transform(data_np)
+        
         cols = data.columns 
         ind = data.index 
         scaled_df = pd.DataFrame(data_np, index=ind, columns=cols)
@@ -43,6 +61,20 @@ class KNNClassifier:
     def find_neighbours(self, k, point, data=None):
         if data is None:
             data = self.data
-        data = data.iloc[:, :len(point)].to_numpy()
+        else: 
+            data.iloc[:,:-1] = self.scale_data(data.iloc[:,:-1], mode="init")
+        point.iloc[:-1] = self.scale_data(point.iloc[:-1], mode="use_old")
+        dists = pd.DataFrame(norm(data.iloc[:,:-1].to_numpy()) - point.iloc[:-1].to_numpy(), columns="Distance")
+        dists["Cluster"] = data["Cluster"]
+        KNN = dists.nsmallest(k, columns="Distance")
+        cluster = KNN["Cluster"].mode() # Find the most common class
+        return cluster
+    
+    
+    
+    
+        
+        
+        
         
         # Takes k neighbours to use, input data to classify and 
